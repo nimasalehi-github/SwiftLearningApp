69b00e61d24d053428ca47398a52f2218f3c023e
n salehi <nimasalehi.auth@icloud.com>
2025-09-27 22:08:27 +0330

    Initial Commit

---
+++ SwiftLearningApp/SwiftLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningAppApp.swift
+//  SwiftLearningApp
+//
+//  Created by Nima Salehi on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
