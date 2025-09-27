32467ffa3ee6ef1fa814e3c8b94045ceca45aee6
n salehi <nimasalehi.auth@icloud.com>
2025-09-27 22:32:29 +0330

    🔎 اینجا چه کار کردیم؟
    یک Stored Property به اسم name ساختیم.
    
    Stored Property یعنی متغیری که توی struct نگه‌داری میشه.
    
    داخل body ازش استفاده کردیم تا متن جدید نشون بدیم.
    
    VStack اضافه کردیم تا بتونیم چندتا متن رو زیر هم نشون بدیم.
    
    👉 حالا کد رو Run کن (با شبیه‌ساز یا روی دستگاه).
    باید ببینی زیر «Hello, world!» یک متن دیگه هم اومده:

--- SwiftLearningApp/ContentView.swift
+++ SwiftLearningApp/ContentView.swift
@@ -5,22 +5,38 @@
 //  Created by Nima Salehi on 9/27/25.
 //
 
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
 import SwiftUI
 
 struct ContentView: View {
+    // Stored Property → مقداری که مستقیم توی حافظه نگه‌داری میشه
+    var name: String = "Nima"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        VStack { // چندتا ویو رو عمودی می‌چینه
             Text("Hello, world!")
+                .padding()
+            
+            // از پراپرتی استفاده می‌کنیم
+            Text("My name is \(name)")
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
